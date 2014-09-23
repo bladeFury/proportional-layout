1d31469c26409c0c173d77d21374862be2066662
zhangge <uniqguang@gmail.com>
2014-09-23 22:00:39 +0800

    added sample module; seems rewrite a layout inflater is not a good choice

--- library/src/main/java/com/wukongtv/proportionallayout/library/PLayoutInflater.java
+++ library/src/main/java/com/wukongtv/proportionallayout/library/PLayoutInflater.java
@@ -1,23 +1,43 @@
 package com.wukongtv.proportionallayout.library;
 
 import android.content.Context;
+import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 
 /**
  * Proportional Layout Inflater
  * Created by zhangge on 14-9-15.
  */
-public class PLayoutInflater extends LayoutInflater{
-    protected PLayoutInflater(Context context) {
-        super(context);
-    }
+public class PLayoutInflater implements LayoutInflater.Factory, LayoutInflater.Factory2 {
 
-    protected PLayoutInflater(LayoutInflater original, Context newContext) {
-        super(original, newContext);
+    private LayoutInflater mDelegate;
+
+    private PLayoutInflater(LayoutInflater l) {
+        mDelegate = l;
+        mDelegate.setFactory2(this);
+    }
+    /**
+     * Obtains the LayoutInflater from the given context.
+     * @param context context
+     * @return the PLayoutInflater
+     */
+    public static PLayoutInflater from(Context context) {
+        LayoutInflater layoutInflater =
+                (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        if (layoutInflater == null) {
+            throw new AssertionError("LayoutInflater not found.");
+        }
+        return new PLayoutInflater(layoutInflater);
     }
 
     @Override
-    public LayoutInflater cloneInContext(Context newContext) {
+    public View onCreateView(String name, Context context, AttributeSet attrs) {
+        return null;
+    }
+
+    @Override
+    public View onCreateView(View parent, String name, Context context, AttributeSet attrs) {
         return null;
     }
 }
